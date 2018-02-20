870234ffbe212a5199a676a62fcd3356d5d18b50
group4pgs <36205075+group4pgs@users.noreply.github.com>
2018-02-20 16:29:44 +0530

    Update py_wall.py

--- py_wall.py
+++ py_wall.py
@@ -10,9 +10,10 @@ urlj = json.load(url)
 
 if urlj['media_type'] == 'image':
 	urllib.urlretrieve(urlj['hdurl'],'/home/raghuttam/apod_img.jpg')
+	#where "/home/raghuttam/apod_img" the image path.. that might change for each system
 else:
 	print("The image is unavailable today as the server has uploaded a "+urlj['media_type'])
 
 os.system('export GIO_EXTRA_MODULES=/usr/lib/x86_64-linux-gnu/gio/modules/')
 os.system('gsettings set org.gnome.desktop.background picture-uri file:///home/raghuttam/apod_img.jpg')
-
+#where "/home/raghuttam/apod_img" the image path.. that might change for each system
